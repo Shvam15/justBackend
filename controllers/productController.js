0a8dd92610596794103c679e879811d5be0c4b72
Shvam15 <158504689+Shvam15@users.noreply.github.com>
2024-10-09 00:39:04 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -509,7 +509,7 @@ export const getSessionId = async (req, res) => {
                 customer_phone: customer_phone,
             },
             order_meta: {
-                return_url: `http://localhost:3000/dashboard/user/orders`,
+                return_url: `https://just-fronted.vercel.app/dashboard/user/orders`,
             },
         };
 
